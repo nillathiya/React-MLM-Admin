993f4f97371b1a5a7e91ee84912fd898fa69176e
ridhdhi <ridhdhibhut3@gmail.com>
2025-02-28 17:58:11 +0530

    Fix: UI Issue

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -22,7 +22,7 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
 
           {/* <!-- ===== Main Content Start ===== --> */}
           <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+            <div className="p-4 md:p-6 2xl:p-10">
               {/* {children} */}
               <Outlet />
             </div>
