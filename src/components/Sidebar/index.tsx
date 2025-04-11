ee618ef0797ba7e7c4e0de714e94186f99230d19
ridhdhi <ridhdhibhut3@gmail.com>
2025-04-11 17:13:43 +0530

    Fix :UserSetting is dynamic

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -121,7 +121,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       }`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
-      <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
+      <div className="flex items-center justify-center  gap-2 px-6 py-5.5 lg:py-6.5 ">
         <NavLink to="/">
           {companyLogo ? (
             <img src={`${API_URL}${companyLogo}`} width={130} height={100} />
